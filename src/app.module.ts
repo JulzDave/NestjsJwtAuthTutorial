3b00d456638d2c42ff008b7336489396c85c5f8a
Julian Dave Shikui <juliandave1989@gmail.com>
2023-02-06 01:48:57 +0200

    add `passport` support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, PassportModule],
   imports: [UsersModule],
 })
 export class AuthModule {}
